cb2b4e967c214576670507b48d78415e77bad4d1
tgunnoe <taylor.gunnoe@iohk.io>
2023-01-31 10:42:45 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+# flake.nix
+{
+  description = "Darktop v2";
+
+  inputs = {
+    std.url = "github:divnix/std";
+    nixpkgs.follows = "std/nixpkgs";
+    nixpkgs-wayland = {
+      url = "github:nix-community/nixpkgs-wayland";
+      inputs.nixpkgs.follows = "std/nixpkgs";
+    };
+    nixgl = {
+      url = "github:guibou/nixGL";
+      inputs.nixpkgs.follows = "std/nixpkgs";
+    };
+  };
+
+  outputs = {
+    std,
+    self,
+    ...
+  } @ inputs:
+    std.growOn {
+      inherit inputs;
+      cellsFrom = ./src;
+      cellBlocks = with std.blockTypes; [
+        (installables "packages" {ci.build = true;})
+        (installables "desktops" {ci.build = true;})
+        (devshells "devshells" {ci.build = true;})
+      ];
+    }
+    {
+      #devShells = std.harvest self ["src" "devshells"];
+      packages = std.harvest self ["darktop" "desktops"];
+    };
+}
