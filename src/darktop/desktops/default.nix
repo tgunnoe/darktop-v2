5cb325bd482859ed79ba5e31131c1c09ec79539a
tgunnoe <taylor.gunnoe@iohk.io>
2023-02-15 16:00:28 -0500

    Test

--- src/darktop/desktops/default.nix
+++ src/darktop/desktops/default.nix
@@ -43,33 +43,33 @@
   };
 
   #container = builtins.path { path = ./container.nix; name = "container"; };
-  container = pkgs.writeText "container" ''
-    {
-      containers.demo = {
-        privateNetwork = true;
-        hostAddress = "10.250.0.1";
-        localAddress = "10.250.0.2";
+  # container = pkgs.writeText "container" ''
+  #   {
+  #     containers.demo = {
+  #       privateNetwork = true;
+  #       hostAddress = "10.250.0.1";
+  #       localAddress = "10.250.0.2";
 
-        config = { pkgs, ... }: {
-          systemd.services.hello = {
-            wantedBy = [ "multi-user.target" ];
-            script = "
-              while true; do
-                echo hello | ${pkgs.netcat}/bin/nc -lN 50
-              done
-            ";
-          };
-          services.tor = {
-            enable = true;
-            client = {
-              enable = true;
-            };
-          };
-          networking.firewall.allowedTCPPorts = [ 50 9050 ];
-        };
-      };
-    }
-  '';
+  #       config = { pkgs, ... }: {
+  #         systemd.services.hello = {
+  #           wantedBy = [ "multi-user.target" ];
+  #           script = "
+  #             while true; do
+  #               echo hello | ${pkgs.netcat}/bin/nc -lN 50
+  #             done
+  #           ";
+  #         };
+  #         services.tor = {
+  #           enable = true;
+  #           client = {
+  #             enable = true;
+  #           };
+  #         };
+  #         networking.firewall.allowedTCPPorts = [ 50 9050 ];
+  #       };
+  #     };
+  #   }
+  # '';
 
   python-with-i3ipc = python-packages:
     with python-packages; [
